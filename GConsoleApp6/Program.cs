d2bc0d36afb8dcea40c69beebad023b4eae0c6fd
Ankam Srinivas <M1064521@mindtree.com>
2021-06-03 12:28:09 +0530

    Add project files.

---
+++ GConsoleApp6/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("hi");
+        }
+    }
+}
